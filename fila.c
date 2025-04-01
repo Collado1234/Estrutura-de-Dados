5329a6880c069bfa776656a096ea828bc89c6ec7
Collado1234 <rennancollado@gmail.com>
2025-03-31 23:09:30 -0300

    Adicionando Comentários

--- fila.c
+++ fila.c
@@ -1,18 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<limits.h>
 
+/*
+    Código que implementa uma fila dinâmica, duplamente encadeada
+    A fila tem como característica principal o FIFO(First in, First Out)
+    @author Rennan Collado
+*/
+
+/*
+Estrutura do Nó que armazenará a informação, por ser duplamente encadeada possui ligação com o próximo nó e com o anterior
+*/
 typedef struct NO{
     int num;
     struct NO* prox;
     struct NO* ant;
 }NO;
-
+/* A fila dinâmica possui a informação do primeiro da fila e do último, além de informação quanto o seu tamanho
+*/
 typedef struct FILA{
     NO* primeiro;
     NO* ultimo;
-    int tam;
+    int tam;   //opcional
 }FILA;
 
+/* Inicializa o Nó recebendo um número inteiro como parâmetro*/
 NO* inicializarNO(int num){
     NO* n = (NO*) malloc(sizeof(NO));
     if(n==NULL){
@@ -20,65 +32,65 @@ NO* inicializarNO(int num){
         return NULL;
     }
     n->num = num;
-    n->prox = NULL;
+    n->prox = NULL;  //Todo nó começa com ponteiros nulos. Evita acessos indevidos na memória
     n->ant = NULL;
     return n;
 }
-
+/* Inicializa fila*/
 FILA* inicializarFila(){
     FILA* f = (FILA*) malloc(sizeof(FILA));
     if(f==NULL){
         printf("\nErro na alocação da fila\n");
         return NULL;
     }
-    f->primeiro = NULL;
+    f->primeiro = NULL;  //todo ponteiro inicializa como nulo. Ajuda na verificação se a fila está vazia ou não
     f->ultimo = NULL;
     f->tam = 0;
     return f;
 }
-
+/* Enfileira o elemento, colocando-o no final de uma fila já existente*/
 void enfileirar(FILA *f,int num){
-    if(f==NULL){
+    if(f==NULL){ //verifica se o ponteiro da fila é valido
         printf("\nNo de fila inválido\n");
         return;
     }
-    NO *novo_no = inicializarNO(num);
-    if(f->primeiro==NULL){
+    NO *novo_no = inicializarNO(num); //inicializa o nó
+    if(f->primeiro==NULL){   // Se a fila estiver vazia o primeiro e ultimo são iguais
         f->primeiro = novo_no;
         f->ultimo = novo_no;
         f->tam++;
-        printf("Primeiro NO inserido na fila\n");
+        // printf("Primeiro NO inserido na fila\n");
         return;
     }
-    f->ultimo->prox = novo_no;
+    f->ultimo->prox = novo_no;    //Coloca no final de uma fila não vazia
     novo_no->ant = f->ultimo;
     f->ultimo = novo_no;
     f->tam++;
 
     return;
 }
-
+/*Desenfileira o primeiro elemento da fila, e retorna seu valor*/
 int desenfileirar(FILA* f){
     int num;
-    NO* noAux = f->primeiro;
+    NO* noAux = f->primeiro;  //noAux serve para não perdermos a referencia do ponto da memória na hora de dar free
     if(noAux == NULL){
         printf("Fila está vazia\n");
-        return -99999;
+        return INT_MIN;
     }
     num = f->primeiro->num;
     f->primeiro = f->primeiro->prox;
     f->primeiro->ant = NULL;
-    free(noAux);
+    free(noAux); //liberando memória alocada
 
     return num;
 }
-
+/*Printa a fila inteira começando pelo início indo até o fim*/
 void percorrerListaInicio(FILA *f){
     if(f==NULL){
         printf("\nNo de fila inválido\n");
         return;
     }
-    NO* noAux = f->primeiro;
+    NO* noAux = f->primeiro;  //Nó auxiliar serve para não perdermos a configuração atual da fila
     int i = 0;
     while(noAux != NULL){
         printf("%d:%d \n",i,noAux->num);
@@ -88,7 +100,7 @@ void percorrerListaInicio(FILA *f){
     printf("\n");
     return;
 }
-
+/* Percorre a lista começando do último*/
 void percorrerListaFinal(FILA *f){
     if(f==NULL){
         printf("\nNo de fila inválido\n");
@@ -102,7 +114,7 @@ void percorrerListaFinal(FILA *f){
     printf("\n");
     return;
 }
-
+/*Remove elemento da fila usando o número que armazena como identificador*/
 void removerElemento(FILA *f, int chave){
     if(f == NULL){
         printf("\nNo de fila inválido\n");
@@ -158,7 +170,7 @@ void removerElemento(FILA *f, int chave){
     printf("Chave %d não encontrada na fila\n", chave);
 }
 
-
+/*Retorna o nó que contém a mesma chave passada por parâmetro, útil para fazer alterações*/
 NO* buscarNo(FILA *f, int chave){
     if(f==NULL){
         printf("\nNo de fila inválido\n");
@@ -176,7 +188,7 @@ NO* buscarNo(FILA *f, int chave){
     printf("Chave nao encontrada\n");
     return NULL;
 }
-
+/*Destrói todos os nós das filas*/
 void liberarFila(FILA *f) {
     NO* atual = f->primeiro;
     while (atual != NULL) {
@@ -186,7 +198,7 @@ void liberarFila(FILA *f) {
     }
     free(f);
 }
-
+/*Insere os elementos na fila de forma ordenada. OBS: Mesma lógica de uma fila de prioridade*/
 void insercaoOrdenada(FILA *f, int novoNum) {
     if (f == NULL) {
         printf("\nNo de fila inválido\n");
@@ -212,6 +224,7 @@ void insercaoOrdenada(FILA *f, int novoNum) {
 
     // Inserção no meio ou final
     NO* noAux = f->primeiro;
+    //Posicionando o ponteiro no lugar correto
     while (noAux->prox != NULL && novoNum >= noAux->num) {
         noAux = noAux->prox;
     }
@@ -232,7 +245,7 @@ void insercaoOrdenada(FILA *f, int novoNum) {
 
     f->tam++;
 }
-
+/*Inserindo elemento em uma posição passada por parâmetro, Range: [0,(tam)]*/
 void inserirFilaIndex(FILA* f, int num, int Index){
     if(f==NULL){
         printf("\nNo de fila inválido\n");
@@ -248,6 +261,7 @@ void inserirFilaIndex(FILA* f, int num, int Index){
         return;  // Caso a alocação falhe
     }
 
+    //Inserindo na posição 0
     if(Index==0){
         printf("Insercao no index 0\n");
         f->primeiro->ant = novoNo;
@@ -290,6 +304,13 @@ void inserirFilaIndex(FILA* f, int num, int Index){
     return;
 }
 
+// Retorna o tamanho da fila
+int tamanhoFila(FILA* f){
+    if(f == NULL) return 0;
+    return f->tam;
+}
+
+/* Main tá bem meia boca*/
 int main (){
     FILA *f = inicializarFila();
     for(int i = 10; i > 0; i--){
@@ -300,13 +321,17 @@ int main (){
 
     inserirFilaIndex(f,110,0);
     inserirFilaIndex(f,100,1);
-    inserirFilaIndex(f,150,4);
+    
     inserirFilaIndex(f,200,10);
     inserirFilaIndex(f,250,13);
     printf("%d\n",f->tam);
     inserirFilaIndex(f,190,f->tam);
 
     percorrerListaInicio(f);
+    percorrerListaFinal(f);
+
+    printf("Tamanho: %d", tamanhoFila(f));
     
     return 0;
 }
+
