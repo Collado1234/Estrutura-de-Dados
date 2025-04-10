1c61449673b8eba18119d3026129928849f92289
Collado1234 <rennancollado@gmail.com>
2025-04-10 18:32:07 -0300

    Add Matriz Transposta, Multiplicacao por Escalar, e comentarios

--- matriz_esparsa.c
+++ matriz_esparsa.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include<limits.h>
 
 #define M 5
 #define N 5
@@ -17,6 +18,9 @@ typedef struct Matriz {
     struct No* col[N];
 } Matriz;
 
+/*
+    alocar memória para um nó informando as coordenadas de onde será inserido na faculdade
+*/
 No* criarNo(int elemento, int linha, int coluna) {
     No* novo_no = (No*) malloc(sizeof(No));
     if (novo_no == NULL) {
@@ -31,6 +35,8 @@ No* criarNo(int elemento, int linha, int coluna) {
     return novo_no;
 }
 
+/* Alocar memória para caber uma matriz e inializar seus ponteiros como nulo
+*/
 Matriz* inicializarMatriz() {
     Matriz* m = (Matriz*) malloc(sizeof(Matriz));
     if (m == NULL) {
@@ -44,6 +50,22 @@ Matriz* inicializarMatriz() {
     return m;
 }
 
+/* Liberar todos os nós e memória alocada da Matriz
+*/
+void liberarMatriz(Matriz* m) {
+    for (int i = 0; i < M; i++) {
+        No* atual = m->lin[i];
+        while (atual != NULL) {
+            No* temp = atual;
+            atual = atual->direita;
+            free(temp);
+        }
+    }
+    free(m);
+}
+
+/* Insere um elemento por vez, cria o nó e insere nas coordenadas
+*/
 void inserirElemento(Matriz* m, int elemento, int linha, int coluna) {
     No* novo_no = criarNo(elemento, linha, coluna);
     if (!novo_no) return;
@@ -91,6 +113,8 @@ void inserirElemento(Matriz* m, int elemento, int linha, int coluna) {
     }
 }
 
+/* Remove o elemento informado na coordenada da matriz
+*/
 void removerElemento(Matriz* m, int linha, int coluna) {
     No *auxCol = m->col[coluna], *antCol = NULL;
     No *auxLin = m->lin[linha], *antLin = NULL;
@@ -127,6 +151,8 @@ void removerElemento(Matriz* m, int linha, int coluna) {
     printf("Elemento [%d][%d] removido\n", linha, coluna);
 }
 
+/* Printa os elementos da MAtriz, colocando 0 nos espaços vazios
+*/
 void printarMatriz(Matriz* m){
     No* auxLin;
     printf("\n");
@@ -145,6 +171,8 @@ void printarMatriz(Matriz* m){
     }
 }
 
+/* Informa a quantidade de elementos presentes na matriz
+*/
 int qtdElementosMatriz(Matriz* m){
     No *auxLin, *auxCol;
     int contX=0,contY=0;
@@ -176,6 +204,8 @@ int qtdElementosMatriz(Matriz* m){
     }
 }
 
+/* Funcao "privada", destinada a ser utilizada por outras funções, retorna o nó da coordenada
+*/
 No* _buscarNo(Matriz* m, int linha, int coluna){
     No* noAux = m->lin[linha];
     while(noAux != NULL && noAux->coluna < coluna){
@@ -187,15 +217,21 @@ No* _buscarNo(Matriz* m, int linha, int coluna){
     return NULL;
 }
 
-void consulta(Matriz *m, int linha, int coluna){
+/* Recebe as coordenadas da matriz e retorna 
+*/
+int consulta(Matriz *m, int linha, int coluna){
     No* no_busca = _buscarNo(m,linha,coluna);
     if(no_busca == NULL){
         printf("Não existe elemento nessa posicao\n");
+        return INT_MIN;
     }else{
         printf("[%d][%d]: %d\n",no_busca->linha, no_busca->coluna,no_busca->elemento);
+        return no_busca->elemento;
     }
 }
-//upseart
+
+/* Altera o valor de um no ja existente, se nao existir, cria um e coloca na posicao
+*/
 void alterar(Matriz* m, int linha, int coluna, int novo_valor){
     No* no_busca = _buscarNo(m,linha,coluna);
     if(no_busca == NULL){
@@ -203,6 +239,36 @@ void alterar(Matriz* m, int linha, int coluna, int novo_valor){
     }else{
         no_busca->elemento = novo_valor;
         printf("Alterado [%d][%d]: %d \n",no_busca->linha,no_busca->coluna,no_busca->elemento);
+    }
+}
+
+/* Retorna um ponteiro para a matriz transposta da recebida por parâmetro
+*/
+Matriz* matrizTransposta(Matriz* original){
+    Matriz* transposta = inicializarMatriz();
+
+    for(int i = 0; i < M; i++){
+        No* aux = original->lin[i];
+
+        while(aux!=NULL){
+            inserirElemento(transposta, aux->elemento, aux->coluna, aux->linha);
+            aux = aux->direita;
+        }
+    }
+    return transposta;
+}
+
+/* Multiplica todos os elementos da matriz por um escalar Alpha
+*/
+void multiplicarPorEscalar(Matriz *m, int alpha){
+    No *noAux;
+
+    for(int i = 0; i < M; i++){
+        noAux = m->lin[i];
+        while(noAux != NULL){
+            noAux->elemento *= alpha;
+            noAux = noAux->direita;
+        }
     }
 }
 
@@ -228,6 +294,13 @@ int main(){
     alterar(m,0,0,8);
     alterar(m,0,1,7);
 
+    printarMatriz(m);
+
+    Matriz* tp = matrizTransposta(m);
+
+    printarMatriz(tp);
+
+    multiplicarPorEscalar(m, 8);
     printarMatriz(m);
 
 return 0;
