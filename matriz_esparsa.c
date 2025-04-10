6d612d220b3f1917c74f0c6e511d7cd855850afe
Collado1234 <rennancollado@gmail.com>
2025-04-10 15:44:18 -0300

    Feat:print e remoção

--- matriz_esparsa.c
+++ matriz_esparsa.c
@@ -90,7 +90,7 @@ void inserirElemento(Matriz* m, int elemento, int linha, int coluna){
     if(auxLin==NULL){
         m->lin[linha] = novo_no;
     }else{
-        while(auxLin == NULL && auxLin->coluna < novo_no->coluna){ //posiciona o nó na posição horizontal correta
+        while(auxLin != NULL && auxLin->coluna < novo_no->coluna){ //posiciona o nó na posição horizontal correta
             anterior = auxLin;
             auxLin = auxLin->direita;
         }
@@ -114,5 +114,93 @@ void inserirElemento(Matriz* m, int elemento, int linha, int coluna){
         }
     }
 }
+
+void removerElemento(Matriz* m, int linha, int coluna){
+    No *auxCol = m->col[coluna];
+    No *auxLin = m->lin[linha];
+    No *anterior_Col = NULL;
+    No *anterior_Lin = NULL;
+
+    while(auxCol != NULL  && auxCol->linha < linha){   //encontrando a linha correta pelo eixo das colunas
+        anterior_Col = auxCol;
+        auxCol = auxCol->baixo;
+    }
+    while(auxLin!=NULL && auxLin->coluna < coluna){   //posicionando a coluna correta pelo eixo das linhas
+        anterior_Lin = auxLin;
+        auxLin = auxLin->direita;
+    }
+
+    if(auxCol == NULL || auxLin == NULL || auxLin->coluna != coluna || auxCol->linha != linha){
+        printf("Nao ha elemento na matriz\n");
+        return;
+    }
+
+    if(anterior_Col == NULL){  //remover no inicio da fila coluna
+        m->col[coluna] = auxCol->baixo;
+        printf("Removeu primeiro elemento da coluna: %d\n", coluna);
+        free(auxCol);
+    }else{
+        anterior_Col->baixo = auxCol->baixo;
+        printf("Remoção na coluna %d, nao e o primeiro\n", coluna);
+        free(auxCol);
+    }
+
+    if(anterior_Lin == NULL){  //remover no inicio da fila coluna
+        m->lin[linha] = auxLin->direita;
+        printf("Removeu primeiro elemento da linha: %d\n", linha);
+        free(auxCol);
+    }else{
+        anterior_Lin->direita = auxLin->direita;
+        printf("Remoção na linha %d, nao e o primeiro\n", linha);
+        free(auxCol);
+    }
+}
+
+void printMatrizLinha(Matriz *m){
+    No*auxLin;
+
+    for(int i = 0; i<M; i++){
+        auxLin = m->lin[i];
+
+        int pos_col = 0;
+
+        if(auxLin == NULL){
+            for(int j = 0; i < M; i++){
+                printf("0 ");
+            }
+        }
+        
+        while(auxLin != NULL){
+            if(auxLin->coluna != pos_col){
+                for(int j = 0; j < auxLin->coluna-pos_col; j++){
+                    printf("0 ");
+                    pos_col++;
+                }
+            }else{
+                printf("%d ", auxLin->elemento);
+                auxLin = auxLin->direita;
+                pos_col++;
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    Matriz* m = inicializarMatriz();
+    inserirElemento(m, 5, 0, 0);
+    inserirElemento(m, 5, 1, 1);
+    inserirElemento(m, 5, 2, 2);
+    inserirElemento(m, 5, 3, 3);
+    inserirElemento(m, 5, 4, 4);
+
+    printMatrizLinha(m);
+
+return 0;
+}
+
+
+
+
 
 
